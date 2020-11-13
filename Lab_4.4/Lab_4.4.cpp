a87f2f6a0dc7658fdc1c009417d602de860944e5
Kiypan <kiypanplay@gmail.com>
2020-11-13 22:41:37 +0200

    Lab_4

---
+++ Lab_4.4/Lab_4.4.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double R, x, xp, xk, dx, y;
+    cout << "R = "; cin >> R;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+
+    cout << fixed;
+    cout << "---------------------------" << endl;
+    cout << "|" << setw(5) << "x" << " |"
+        << setw(7) << "y" << " |" << endl;
+    cout << "---------------------------" << endl;
+
+    for (x = xp; x <= xk; x += dx) {
+        if (x <= -2)
+            y = 1;
+        else
+            if (-2 < x && x <= 0)
+                y = -x / 2.;
+            else
+                if (0 < x && x <= R)
+                    y = R - sqrt(R * R - x * x);
+                else
+                    if (R < x && x <= 2 * R)
+                        y = sqrt(R * R - (x - R) * (x - R));
+                    else
+                        y = -(x - 2 * R) / (6 - 2 * R);
+        cout << "|" << setw(7) << setprecision(2) << x
+            << " |" << setw(10) << setprecision(3) << y
+            << " |" << endl;
+    }
+
+    cout << "---------------------------" << endl;
+
+    return 0;
+}
---
+++ Lab_4.6/Lab_4.6.cpp
@@ -0,0 +1,69 @@
+﻿#include<iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	double P, S;
+	int k, i;
+	S = 1;
+	i = 5;
+	while (i <= 25)
+
+	{
+		P = 0;
+		k = 1;
+		while (k <= i * i)
+
+		{
+			P *= i;
+			k++;
+
+		}
+		S += sqrt(i * i) + P / i;
+		i++;
+
+	}
+	cout << S << endl;
+	S = 1;
+	i = 5;
+	do
+	{
+		P = 0;
+		k = 1;
+		do
+		{
+			P *= i;
+			k++;
+		} while (k <= i * i);
+		S += sqrt(i * i) + P / i;
+		i++;
+	} while (i <= 25);
+	cout << P << endl;
+	S = 1;
+	for (i = 5; i <= 25; i++)
+
+	{
+		P = 0;
+		for (k = 1; k <= i * i; k++)
+
+		{
+			P *= i;
+
+		}
+		S += sqrt(i * i) + P / i;
+
+	}
+	cout << P << endl;
+	S = 1;
+	for (i = 25; i >= 5; i--)
+
+	{
+		P = 0;
+		for (k = i * i; k >= 1; k--)
+		{
+			P *= i;
+		}
+		S += sqrt(i * i) + P / i;
+	}
+	cout << P << endl;
+	return 0;
+}
